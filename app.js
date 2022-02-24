96b49aa989b318ab1abda69de6d6cb90f13a0555
Rahulk612 <rahulkonda612@gmail.com>
2022-02-24 13:13:12 +0530

    deploye

--- app.js
+++ app.js
@@ -8,12 +8,36 @@ const Seettes = require("./src/modules/Products/SeettesModel")
 
 const Beds = require("./src/modules/Products/BedsModel")
 
+const Home = require("./src/modules/Products/wfhModel")
+
+const Lamps = require("./src/modules/Products/lampsModel")
 
 app.get("/pepperfry/seettes", async (req, res) => {
   try {
     const products = await Seettes.find();
 
     res.status(200).send(products);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+
+app.get("/pepperfry/workfromhome",async(req,res)=>{
+  try {
+    const home = await Home.find();
+
+    res.status(200).send(home);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.get("/pepperfry/lamps", async (req, res) => {
+  try {
+    const lamps = await Lamps.find();
+
+    res.status(200).send(lamps);
   } catch (err) {
     res.status(500).send(err.message);
   }
---
+++ src/modules/Products/wfhModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const workFromHomeSchema = new mongoose.Schema(
+  {
+    Title: { type: String, required: true },
+    orgPrice: { type: Number, required: true },
+    offer: { type: Number, required: true },
+    img1: { type: String, required: true },
+    img2: { type: String, required: true },
+    img3: { type: String, required: true },
+    Brand: { type: String, required: true },
+    Dimensions: { type: String, required: true },
+    dims: { type: Array, required: true },
+    Weight: { type: String, required: true },
+    Colour: { type: String, required: true },
+    Warranty: { type: String, required: true },
+    Assembly: { type: String, required: true },
+    Primary_Material: { type: String, required: true },
+    Room_Type: { type: String, required: true },
+    Seating_Height: { type: String, required: true },
+    cashback: { type: Number, required: true },
+    shipping: { type: Number, required: true },
+    limited: { type: Boolean, required: true },
+    rating: { type: Number, required: true },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("workfromhome", workFromHomeSchema);
