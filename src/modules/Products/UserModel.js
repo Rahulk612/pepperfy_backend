191993b9bcf86b43d9d83a55a3a253db19158f43
Rahulk612 <rahulkonda612@gmail.com>
2022-02-25 18:09:51 +0530

    deploye

--- src/modules/Products/UserModel.js
+++ src/modules/Products/UserModel.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs")
 const UserRegisterSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    number: { type: Number, required: true },
+    number: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
