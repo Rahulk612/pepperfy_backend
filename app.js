929bb81730745202ac1535ff8768bfad6775ceba
Rahulk612 <rahulkonda612@gmail.com>
2022-03-23 17:46:40 +0530

    cors

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+
+
 
 app.use(express.json());
 app.use(cors());
